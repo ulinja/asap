c3908f3bf39a4cb11610d0467f413cf1082735f2
Cybork <j.lobbes@tu-braunschweig.de>
2021-04-18 15:50:11 +0200

    Added fzf navigation functions to cd into folders.

--- saplib/build/saplib/fish/saplib.fish
+++ saplib/build/saplib/fish/saplib.fish
@@ -1,5 +1,13 @@
-# Wrapper script you may use to source all of saplib's fish functions
+# Wrapper script to load saplib's fish configuration
 
+# Disable the greeting message
+function fish_greeting
+end
+
+# Enable vi-mode by default
+fish_vi_key_bindings
+
+# Source all of saplib's fish functions
 for scriptfile in /usr/local/lib/saplib/fish/src/*.fish
         source $scriptfile
 end
--- saplib/build/saplib/fish/src/navigation.fish
+++ saplib/build/saplib/fish/src/navigation.fish
@@ -12,3 +12,12 @@ end
 function la --wraps exa --description "Lists all (including hidden) files in the current (or specified) directory in a detailed listing."
         exa --long --header --group --classify --git --icons --group-directories-first --all $argv
 end
+
+function fcd --description "Uses 'fzf' to list and cd into directories under the current directory."
+        cd (du . 2>/dev/null | awk '{print $2}' | sed 's/^\.\// /' | fzf --preview 'exa --oneline --classify --icons --all --group-directories-first {2}' | awk '{print $2}')
+end
+
+function fcda --description "Uses 'fzf' to list any accessible directories on the system and cd into them."
+        cd (du / 2>/dev/null | awk '{print $2}' | sed 's/^/ /' | fzf --preview 'exa --oneline --classify --icons --all --group-directories-first {2}' | awk '{prin
+t $2}')
+end
